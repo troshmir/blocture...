48a2739a9f558dfc81e7e0a23de38eb3fdae2eae
Natasa Grabovac <tasa.grabovac@gmail.com>
2019-10-21 15:23:41 +0200

    installed openzeppelin, created empty Image.sol with imported ERC721 contract

---
+++ contracts/Image.sol
@@ -0,0 +1,4 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
--- truffle-config.js
+++ truffle-config.js
@@ -25,6 +25,12 @@
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
+  compilers: {
+    solc: {
+      version: "0.5.2"
+    }
+  },
+
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
